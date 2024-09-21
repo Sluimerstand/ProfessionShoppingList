69d7a0846f75d5be63d2a0f8a43bf04246e7bd73
Sluimerstand <Ruben.Sluimer@gmail.com>
2024-09-21 22:22:31 +0200

    Tier 2 materials no longer also add a tier 3 listing when making a shopping list. The intent was to account for if tier 3 mats are cheaper, but it shows up as too confusing in the shopping list.

--- modules/AuctionHouse.lua
+++ modules/AuctionHouse.lua
@@ -93,10 +93,6 @@ function app.CreateAuctionatorButton()
 						-- Put the items in the temporary variable
 						if reagentCount > 0 then
 							table.insert(searchStrings, Auctionator.API.v1.ConvertToSearchString(app.Name, { searchString = itemName, isExact = true, categoryKey = "", tier = reagentQuality, quantity = reagentCount}))
-							if reagentQuality == 2 then
-								-- Also add tier 3 for minimum reagent quality 2
-								table.insert(searchStrings, Auctionator.API.v1.ConvertToSearchString(app.Name, { searchString = itemName, isExact = true, categoryKey = "", tier = 3, quantity = reagentCount}))
-							end
 						end
 					end
 				end
